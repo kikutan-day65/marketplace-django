23aeee9c43db60dff773a1a7ba3baa9703fccb51
Haruto Mori <baystarsman@outlook.jp>
2023-03-25 12:26:16 +0100

    make images be able to display

--- core/urls.py
+++ core/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
@@ -8,4 +10,4 @@ urlpatterns = [
 
     path('', views.index, name='index'),
     path('contact/', views.contact, name='contact'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)   # to display the images
