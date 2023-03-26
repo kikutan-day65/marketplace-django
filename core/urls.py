ee0d9d327681d3459a8cd137fb6d975607d59521
Haruto Mori <baystarsman@outlook.jp>
2023-03-26 21:36:53 +0200

    create a sign up page

--- core/urls.py
+++ core/urls.py
@@ -5,9 +5,12 @@ from django.urls import path
 
 from . import views
 
+app_name = 'core'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('', views.index, name='index'),
     path('contact/', views.contact, name='contact'),
+    path('signup/', views.signup, name='signup'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)   # to display the images
