b123bb3c30b6224c1cbef454bcc5950b25a6f9ea
Haruto Mori <baystarsman@outlook.jp>
2023-03-27 23:53:48 +0200

    build a login page

--- core/urls.py
+++ core/urls.py
@@ -2,8 +2,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from . import views
+from .forms import LoginForm
 
 app_name = 'core'
 
@@ -13,4 +15,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('contact/', views.contact, name='contact'),
     path('signup/', views.signup, name='signup'),
+    path('login/', auth_views.LoginView.as_view(template_name='core/login.html', authentication_form=LoginForm), name='login'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)   # to display the images
