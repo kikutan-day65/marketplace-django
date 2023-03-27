71d4f23b8da9c30dd88ce80226ba4d053a44aa5d
Haruto Mori <baystarsman@outlook.jp>
2023-03-28 00:29:50 +0200

    create a dashboard page

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+from item.models import Item
 
 # Create your views here.
+
+def index(request):
+    items = Item.objects.filter(created_by=request.user)
+    context = {
+        'items': items,
+    }
+    return render(request, 'dashboard/index.html', context)
--- puddle/urls.py
+++ puddle/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     
     path('', include('core.urls')),
     path('items/', include('item.urls')),
+    path('dashboard/', include('dashboard.urls'))
 ]
