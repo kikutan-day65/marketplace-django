fe455056eb166ab5871db638b4d61acbcf8902a6
Haruto Mori <baystarsman@outlook.jp>
2023-03-28 20:25:31 +0200

    build a function to search items

--- item/views.py
+++ item/views.py
@@ -1,10 +1,34 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 
 from .forms import NewItemForm, EditItemForm
-from .models import Item
+from .models import Item, Category
 
 # Create your views here.
+
+def items(request):
+    query = request.GET.get('query', '')
+    category_id = request.GET.get('category', 0)
+    categories = Category.objects.all()
+    items = Item.objects.filter(is_sold=False)
+
+    if category_id:
+        items = items.filter(category_id=category_id)
+
+    if query:
+        items = items.filter(
+            Q(name__icontains=query),
+            Q(descriptions__icontains=query),
+        )
+
+    context = {
+        'items': items,
+        'query': query,
+        'categories': categories,
+        'category_id': int(category_id),
+    }
+    return render(request, 'item/items.html', context)
 
 def detail(request, pk):
     item = get_object_or_404(Item, pk=pk)
